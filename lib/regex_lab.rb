29803634dbe7194fea6834ec3d88a3487d9417bb
Andy Maskiell <maskiella@gmail.com>
2017-12-20 22:50:12 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -6,7 +6,8 @@ def starts_with_a_vowel?(word)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-  text =~ /^[un]+$[ing]/
+  if text > 0
+    text.match(/^[un]+$[ing]/)
 end
 
 def words_five_letters_long(text)
