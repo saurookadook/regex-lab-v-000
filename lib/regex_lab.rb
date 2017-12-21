5939a3ce94a7bdb5a24f6419210ceef7c1ffb1fe
Andy Maskiell <maskiella@gmail.com>
2017-12-21 01:12:54 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,28 @@
-def starts_with_a_vowel?(word)
+require 'pry'
 
+def starts_with_a_vowel?(word)
+  # binding.pry
+  !(word =~ /^[aeiou]/i).nil?
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.scan(/un\S*ing\b/)
 end
 
 def words_five_letters_long(text)
-
+  text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  !(text =~ /^\p{Lu}.*\p{P}$/).nil?
 end
 
 def valid_phone_number?(phone)
-
+  phone =~ /([0-9] *?){10}|([0-9] *?){4}/
+  # phone =~ /([0-9] *?){10}/|/([0-9]{3})([0-9{7}])
+  # if !(phone =~ /\w+/).nil?
+  #   false
+  # elsif !(phone =~ /\d{1,10}/).nil?
+  #   true
+  # end
 end
