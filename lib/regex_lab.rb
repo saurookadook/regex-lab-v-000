9281fe93ad66054503dc4be45bb4687179f10aae
Andy Maskiell <maskiella@gmail.com>
2017-12-21 00:13:11 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -18,5 +18,6 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-  !(phone =~ /\d\W/).nil?
+  !(phone =~ /\p{Pi}\d{10}\p{Pf}/)
+  # !(phone =~ /\d\W/).nil?
 end
